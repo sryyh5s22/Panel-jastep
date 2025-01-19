46ba7c451550387f68943335fa50d5f5901d2f2b
sryyh5s22 <serverportugal1222@gmail.com>
2025-01-19 11:08:55 +0700

    Delete delete.php

--- delete.php
+++
@@ -1,9 +0,0 @@
-<?php
-$read = file_get_contents('data.json');
-$json = json_decode($read,true);
-
-unset($json[$_GET['keys']]);
-$put = fopen("data.json","w") or die("Cannot write to path");
-		 fwrite($put,json_encode(array_values($json)));
-		 fclose($put);
-echo '200';
